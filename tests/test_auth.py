2f51bc9df0744fdbf139aaf4760ad06e43e891b2
zaqaqaq <podushkin.den@mail.ru>
2026-08-11 20:22:38 +0700

    Подключил allure, написал тесты на регистрацию авторизацию

---
+++ tests/test_auth.py
@@ -0,0 +1,43 @@
+import allure
+
+
+def test_negative_data_auth(page):
+    """ввод неверных данных"""
+    page.locator('text=Войти').first.click()
+    page.fill('input[type="email"]', 'zxcmode@zxc.ru')
+    page.fill('input[type="password"]', '22222')
+    page.click('.btn-primary')
+    assert page.locator('text="Ошибка входа. Проверьте email и пароль"').is_visible()
+
+@allure.feature("Авторизация")
+@allure.story("Успешный вход")
+@allure.severity(allure.severity_level.CRITICAL)
+def test_positiv_auth(page):
+    """Вход с валидными данными"""
+    page.locator('text="Войти"').first.click()
+    page.fill('.input','1@test.ru')
+    page.fill('input[type="password"]','1234567')
+    page.click('.btn-primary')
+    assert page.url == "http://localhost:3000/"
+    assert page.locator('.hidden').is_visible()
+
+def test_logout(page):
+    """проверка входа и выхода из системы"""
+    page.locator('text="Войти"').first.click()
+    page.fill('.input','1@test.ru')
+    page.fill('input[type="password"]','1234567')
+    page.click('.btn-primary')
+    page.locator('.hidden').click()
+    page.click('.btn-danger')
+    assert page.locator('a:has-text("Войти")')
+
+def test_registration_positive(page):
+    """Регистрация пользователя"""
+    page.locator('text="Войти"').first.click()
+    page.click('text="Зарегистрируйтесь"')
+    page.fill('input[type="email"]', 'dsff@mail.ru')
+    page.fill('input[type="password"]', '123456')
+    page.locator('input[type="text"]').nth(1).fill('Оывв')
+    page.locator('text="Зарегистрироваться"').click()
+
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,8 @@
 def test_main_opens(page):
     """Проверка открытия главной страницы"""
     assert page.locator('text="Готовьте с умом, экономьте с нами"').is_visible()
+
+def test_auth_main(page):
+    """Проверка кнопки войти"""
+    page.locator('text=Войти').first.click()
+
